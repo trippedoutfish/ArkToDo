cf2b34f2dabf839e43246d844e22b5cefb31909d
trippedoutfish <trippedoutfish@gmail.com>
2022-01-22 21:51:50 -0800

    Basic updates

---
+++ ArkToDo/ArkToDoMaui/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace ArkToDoMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = $"Currrent count: {count}";
+
+            SemanticScreenReader.Announce(CounterLabel.Text);
+        }
+    }
+}

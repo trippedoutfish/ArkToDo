86d52711e3b7a8a3e76f639ed317f8fef928acca
trippedoutfish <trippedoutfish@gmail.com>
2022-01-22 17:38:48 -0800

    Init

---
+++ ArkToDo/ArkToDo/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ArkToDo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ArkToDo/ArkToDo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ArkToDo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
